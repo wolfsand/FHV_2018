d8f4f5787918499beac8798baeda8eee9df7017b
wolfsand <35975804+wolfsand@users.noreply.github.com>
2018-10-10 09:07:35 +0200

    Update CompareSortAlgorithms/src/Settings.java

--- CompareSortAlgorithms/src/Settings.java
+++ CompareSortAlgorithms/src/Settings.java
@@ -4,51 +4,48 @@ import java.io.*;
 import java.util.*;
 
 public class Settings {
-	
+
 	private static Settings settings; /* single object */
-	
-	
+
+
 	Properties properties = new Properties();
 
-	
+
 	/* empty constructor - singleton pattern */
 	private Settings() {		
 	}
-	
+
 	public static Settings getInstance(String[] args) {
 		if (settings == null) {
 			settings = new Settings();
 			settings.interpreteArgs(args);	
-			System.out.println("Hello");
+			System.out.println("settings read");
 		}
 
 		return settings;
 	}
-	
+
 	/* first argument ==> filename of settings */
 	private void interpreteArgs(String[] args) {
 		String filename;
-		File _file;
-		System.out.println("Hello2");
+		File file;
 		if (args.length > 0) {
-			System.out.println("Hello3");
 			filename = args[0];		
 		}
 		else filename ="settings.txt";
-		
-		System.out.println("Filename: "+filename);
-		
-		_file = new File(filename);	
-		if (_file.exists() && _file.canRead()) {
-			setProperties(_file);
 
+		System.out.println("Filename: "+filename);
+
+		file = new File(filename);	
+		if (file.exists() && file.canRead()) {
+			setProperties(file);
 		};
-		saveProperties(_file);
-		
+		// saveProperties(file);
+
 	}
-	
+
 	private void setProperties(File file) {
-		
+
 		try {
 			Reader reader = new FileReader(file);
 			System.out.println(file.getAbsolutePath());
@@ -59,9 +56,9 @@ public class Settings {
 			System.err.println("Could not find file");
 		}
 		catch (IOException e) {
-		System.err.println("error opening file"); }
+			System.err.println("error opening file"); }
 	}
-	
+
 	private void saveProperties(File file) {
 		try {
 			Writer writer = new FileWriter(file);
@@ -74,9 +71,9 @@ public class Settings {
 			System.err.println("Could not find file");
 		}
 		catch (IOException e) {
-		System.err.println("error opening file"); }
+			System.err.println("error opening file"); }
 	}
-		
+
 
 	public int getArrayLength() {
 		try {
@@ -86,31 +83,30 @@ public class Settings {
 			return 0;
 		}
 	}
-	
+
 	public SortStrategy getSortStrategy() {
 
-			String sortMethod;
-			sortMethod = properties.getProperty("SortMethod");
-			if (sortMethod == "SelectionSort") {
-					return new SelectionSort();
-			}
-			else {
-				return new SelectionSort();
-			}
+		String sortMethod;
+		sortMethod = properties.getProperty("SortMethod");
+		if (sortMethod == "SelectionSort") {
+			return new SelectionSort();
+		}
+		else {
+			return new SelectionSort();
+		}
 	}
-	
+
 	public FillStrategy getFillStrategy() {
 
 		String fillMethod;
 		fillMethod = properties.getProperty("FillMethod");
 		if (fillMethod == "Random") {
-				return new FillRandom();
+			return new FillRandom();
 		}
 		else {
 			return new FillRandom();
 		}
-}
-
+	}
 
 
 }
